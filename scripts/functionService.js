235263a09ce20be215f506ddd57c53b73aa94b55
swalsh <steven.walsh@zizo.co.uk>
2016-05-17 09:25:55 +0100

    updated some scss. updated FunctionService.

--- scripts/functionService.js
+++ scripts/functionService.js
@@ -1,52 +1,65 @@
 ﻿(function () {
-
     'use strict';
-    angular.module('app').factory('FunctionService', function () {
-        return {
-            lowerFirstLetter: lowerFirstLetter,
-            findWithAttr: findWithAttr,
-            friendlyErrorBuilder: friendlyErrorBuilder,
-            compareString: compareString
-        }
-        
-        function lowerFirstLetter(string) {
+
+    angular.module('app').factory('FunctionService', ['$q', function ($q) {
+        var service = this;
+
+        service.commonValues = function () {
+            return {
+                bands: { 'High': '#41a891', 'Medium': '#a044c9', 'Low': '#dbad5e' }
+            };
+        };
+
+        service.lowerFirstLetter = function(string) {
             return string.charAt(0).toLowerCase() + string.slice(1);
         }
 
         //Find object using attr of value in array.
-        function findWithAttr(array, attr, value) {
+        service.findWithAttr = function(array, attr, value) {
             for (var i = 0; i < array.length; i += 1) {
                 if (array[i][attr] === value) {
                     return i;
                 }
             }
             return -1;  //when doesn't exist.
-        };
-
-        function compareString(a, b, sensitive) {
-            if (sensitive)  return a.toLowerCase() === b.toLowerCase();
-            if (!sensitive) return a === b;
-        };
-
-        function friendlyErrorBuilder(res) {
-            var idx, title, error,
-                details = res.error.details,
-                message = res.error.message;
-            if (details === null) {
-                //idx = message.lastIndexOf(')');
-                title = 'Error';
-                error = message;
-                //error = (idx === -1) ? message :
-                //                       message.substring(idx + 1);
-            } else {
-                //idx = details.lastIndexOf(')');
-                title = message;
-                error = details;
-                //error = (idx === -1) ? details :
-                //                       details.substring(idx + 1);
-            }
-            return { title: title, message: error };
         }
 
-    });
+        service.compareString = function (a, b, sensitive) {
+            if (sensitive) return a.toLowerCase() === b.toLowerCase();
+            if (!sensitive) return a === b;
+        }
+
+        //Separates items of 1 array into several based on values of given properties.
+        service.groupItemsByProperties = function (array, groupProperties) {
+            return $q(function (resolve, reject) {
+                var groupedArrays = groupBy(array, function (item) {
+                    return getGroupProperties(item, groupProperties);
+                });
+                resolve(groupedArrays);
+            });
+        }
+
+        //Retrieve the item values for grouping by
+        function getGroupProperties(item, groups) {
+            var array = [];
+            angular.forEach(groups, function (group) {
+                array.push(item[group]);
+            });
+            return array;
+        }
+        //Group the items into arrays using the values.
+        function groupBy(array, f) {
+            var groups = {};
+            array.forEach(function (o) {
+                var group = JSON.stringify(f(o));
+                groups[group] = groups[group] || [];
+                groups[group].push(o);
+            });
+            return Object.keys(groups).map(function (group) {
+                return groups[group];
+            })
+        }
+
+        return service;
+    }]);
 })()
