c420a5087dcc1744fd3a2bd2be8ca611c415f5bc
bakuzan <imannoyed4days@hotmail.co.uk>
2016-10-18 13:11:08 +0100

    added xmlToJson function

---
+++ scripts/xmlToJson.js
@@ -0,0 +1,38 @@
+function xmlToJson(xml) {
+	var ELEMENT_NODE_TYPE = 1,
+	    TEXT_NODE_TYPE = 3,
+	    obj = {};
+
+	if (xml.nodeType == ELEMENT_NODE_TYPE) {
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == TEXT_NODE_TYPE) {
+		obj = xml.nodeValue;
+	}
+
+	if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+		obj = xml.childNodes[0].nodeValue;
+	}
+	else if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i),
+			    nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+}
