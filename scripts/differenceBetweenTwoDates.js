418f95c4417567d7a6de49102c39ec3b9e0cf39e
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-16 11:28:10 +0000

    Update differenceBetweenTwoDates.js

--- scripts/differenceBetweenTwoDates.js
+++ scripts/differenceBetweenTwoDates.js
@@ -1,10 +1,10 @@
 function treatAsUTC(date) {
-    var result = new Date(date);
+    let result = new Date(date);
     result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
     return result;
 }
 
 function daysBetween(startDate, endDate) {
-    var millisecondsPerDay = 24 * 60 * 60 * 1000;
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
     return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay;
 }
